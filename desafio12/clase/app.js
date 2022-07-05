d837893edddce9dab4f8c5316f109115b45a1aa0
Aldana <aldanamilesse@gmail.com>
2022-07-04 21:56:45 -0300

    desafio de clase actulizado

--- desafio12/clase/app.js
+++ desafio12/clase/app.js
@@ -7,14 +7,19 @@ console.log(valorDos);
 let valorTres = Number(prompt("Ingrese un tercer numero"));
 console.log(valorTres);
 
+let resultado = document.getElementById("resultado")
+
 if(valorUno > valorDos && valorUno > valorTres){
     console.log("el numero mayor es el valor 1: " + valorUno);
+    resultado.innerHTML = valorUno;
 }
 else if(valorDos > valorUno && valorDos > valorTres){
     console.log("el numero mayor es el valor 2: " + valorDos);
+    resultado.innerHTML = valorDos;
 }
 else if(valorTres > valorUno && valorTres > valorDos){
     console.log("el numero mayor es el valor 3: " + valorTres);
+    resultado.innerHTML = valorTres;
 }
 else{
     console.log("hola")
